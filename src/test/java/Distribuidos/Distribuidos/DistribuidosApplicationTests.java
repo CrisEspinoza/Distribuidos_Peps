34f71728ca4da6dfa4c054c145eb6b08ae620ca9
CrisEspinoza <cristian.espinoza.s@usach.cl>
2020-06-15 00:31:58 -0400

    ADD: Agregando proyecto base

---
+++ src/test/java/Distribuidos/Distribuidos/DistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package Distribuidos.Distribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
